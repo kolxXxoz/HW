4ddcc0f1377fe96811228329ddda5ac3ddb029bc
Seleznev.Semen@urfu.me <Seleznev.Semen@urfu.me>
2021-11-15 03:08:06 +0500

    hw7

---
+++ hw7/hw7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine(Function24(x));
+            Console.ReadKey();
+        }
+
+        static double Function24(double x)
+        {
+            if (x < 2)
+                return Math.Log10(2 - x);
+            else if (x > 3)
+                return Math.Log10(x - 3);
+            else return 1;
+        }
+    }
+}
