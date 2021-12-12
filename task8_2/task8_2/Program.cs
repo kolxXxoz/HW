6de5fa0ed6dcf6f0ee48765bc9971579ccb506b4
Seleznev.Semen@urfu.me <Seleznev.Semen@urfu.me>
2021-12-13 00:08:24 +0500

    task 8

---
+++ task8_2/task8_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите желаемое количество чисел");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write($"a1 = ");
+            
+            double summa = double.Parse(Console.ReadLine());
+            double temp = summa;
+
+            for (var i = 1; i < n; i++)
+            {
+                Console.Write($"a{i+1} = ");
+                temp *= double.Parse(Console.ReadLine());
+                summa += temp;
+            }
+
+            Console.WriteLine($"x = {summa}");
+
+            Console.ReadKey();
+        }
+    }
+}
