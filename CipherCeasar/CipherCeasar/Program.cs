f42aa0e66fb6810a8d85e2bb86bb99948358c593
Seleznev.Semen@urfu.me <Seleznev.Semen@urfu.me>
2021-12-30 12:27:15 +0500

    0

---
+++ CipherCeasar/CipherCeasar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherCeasar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine().ToUpper();
+
+            Console.Write("Введите числовой ключ: ");
+            int key = int.Parse(Console.ReadLine());
+
+            string cipher = Encrypt(text, key);
+            Console.WriteLine(cipher);
+            
+            Console.WriteLine(Decrypt(cipher, key));
+
+            Console.ReadKey();
+        }
+
+        static string Encrypt(string text, int key)
+        {
+            string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            var alphabetLenght = alphabet.Length;
+            var cipher = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var index = alphabet.IndexOf(text[i]);
+
+                if (index < 0)
+                    cipher += text[i];
+
+                else
+                {
+                    var newIndex = (alphabetLenght + index + key) % alphabetLenght;
+                    cipher += alphabet[newIndex];
+                }
+            }
+
+            return cipher;
+        }
+
+        static string Decrypt(string text, int key)
+        {
+            return Encrypt(text, -key);
+        }
+    }
+}
+
+
---
+++ CipherVernam/CipherVernam/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherVernam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine().ToUpper().Replace(" ", "");
+
+            var key = GenerateKey(text);
+            Console.WriteLine($"Ваш ключ: {key}");
+
+            string cipher = Encrypt(text, key);
+            Console.WriteLine(cipher);
+
+            Console.WriteLine(Decrypt(cipher, key));
+
+            Console.ReadKey();
+        }
+
+        static string GenerateKey(string text)
+        {
+            string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            var alphabetLenght = alphabet.Length;
+            string key = "";
+            Random rand = new Random();
+
+            for (var i = 0; i < text.Length; i++)
+                key += alphabet[rand.Next(0, alphabetLenght)];
+
+            return key;
+        }
+
+        static string Encrypt(string text, string key)
+        {
+            string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            var alphabetLenght = alphabet.Length;
+            var cipher = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var textIndex = alphabet.IndexOf(text[i]);
+                var keyIndex = alphabet.IndexOf(key[i]);
+
+                if (textIndex < 0)
+                    cipher += text[i];
+
+                else
+                {
+                    var cipherIndex = (alphabetLenght + textIndex + keyIndex) % alphabetLenght;
+                    cipher += alphabet[cipherIndex];
+                }
+            }
+
+            return cipher;
+        }
+
+        static string Decrypt(string cipher, string key)
+        {
+            while (key.Length < cipher.Length)
+                key += key;
+
+            string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            var alphabetLenght = alphabet.Length;
+            var text = "";
+
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                var cipherIndex = alphabet.IndexOf(cipher[i]);
+                var keyIndex = alphabet.IndexOf(key[i]);
+
+                if (cipherIndex < 0)
+                    text += cipher[i];
+
+                else
+                {
+                    var textIndex = (alphabetLenght + cipherIndex - keyIndex) % alphabetLenght;
+                    text += alphabet[textIndex];
+                }
+            }
+
+            return text;
+        }
+    }
+}
